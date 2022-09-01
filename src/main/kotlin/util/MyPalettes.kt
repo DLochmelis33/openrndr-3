91f4e3618cc0870c94eff482b7ff122e94bf2155
dlochmelis33 <dlochmelis@gmail.com>
2022-09-01 17:03:06 +0200

    idk

--- src/main/kotlin/util/MyPalettes.kt
+++ src/main/kotlin/util/MyPalettes.kt
@@ -16,6 +16,6 @@ fun ColorRGBa.shiftHue(degrees: Double) = toHSVa().shiftHue(degrees).toRGBa()
 fun randomPastel(r: Random = Random): ColorHSVa {
     val rangeH = 0.0..360.0
     val rangeS = 0.1..0.5
-    val rangeV = 0.5..1.0
+    val rangeV = 0.2..1.0
     return ColorHSVa(r.nextDouble(rangeH), r.nextDouble(rangeS), r.nextDouble(rangeV), 1.0)
 }
