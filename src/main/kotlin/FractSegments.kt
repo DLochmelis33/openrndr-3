e01b3a6658dd8dc075c9de1cbe99aed37def2769
dlochmelis33 <dlochmelis@gmail.com>
2023-02-18 22:39:10 +0100

    fract hehe

--- src/main/kotlin/FractSegments.kt
+++ src/main/kotlin/FractSegments.kt
@@ -1,18 +1,19 @@
+import org.openrndr.KEY_SPACEBAR
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.isolated
 import org.openrndr.math.Vector2
 import org.openrndr.shape.Segment
 import kotlin.math.sqrt
+import kotlin.random.Random
 
 interface Fract {
     fun fract(seg: Segment): List<Segment>
 }
 
 val koch = object : Fract {
-    private val lenCoef = 1.0 / (2.0 + sqrt(2.0))
+    private val coef = 1.0 / (2.0 + sqrt(2.0))
     override fun fract(seg: Segment): List<Segment> = seg.run {
-        val coef = 1.0 - sqrt(2.0) / 2.0
         val base1 = (end - start) * coef
         val base2 = -Vector2(-base1.y, base1.x)
 
@@ -25,22 +26,55 @@ val koch = object : Fract {
     }
 }
 
+val random = Random
+fun makeRs() = generateSequence { random.nextDouble(0.0, 2.0) }.take(100).toList()
+var rs = makeRs()
+
+val hehe = object : Fract {
+
+    override fun fract(seg: Segment): List<Segment> = seg.run {
+        val base1 = (end - start) / 4.0
+        val base2 = -Vector2(-base1.y, base1.x)
+
+        val iter = rs.iterator()
+        fun r() = iter.next()
+
+        val p0 = start
+        val p1 = p0 + base1 * r() - base2 * r()
+        val p2 = p0 + base1 * (2.0 + r()) + base2 * r()
+        val p3 = end
+
+        listOf(p0, p1, p2, p3).zipWithNext { a, b -> Segment(a, b) }
+    }
+}
+
 fun main() = application {
     configure {
         width = 1080
         height = 720
     }
     program {
-        val fract: Fract = koch
-        val initial = Segment(Vector2(width * 0.2, height * 0.3), Vector2(width * 0.8, height * 0.7))
-        var segs = listOf(initial)
-        repeat(5) {
-            segs = segs.flatMap { fract.fract(it) }
+        val fract: Fract = hehe
+        val initial = Segment(Vector2(width * 0.2, height * 0.5), Vector2(width * 0.8, height * 0.5))
+        lateinit var segs: List<Segment>
+        fun calc() {
+            rs = makeRs()
+            segs = listOf(initial)
+            repeat(8) {
+                segs = segs.flatMap { fract.fract(it) }
+            }
+        }
+        calc()
+        keyboard.keyDown.listen {
+            if (it.key == KEY_SPACEBAR) {
+                calc()
+            }
         }
         extend {
             drawer.isolated {
                 clear(ColorRGBa.WHITE)
                 for (s in segs) segment(s)
+//                segment(initial)
             }
         }
     }
