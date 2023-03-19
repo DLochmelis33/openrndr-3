9b2bf2d765a6478a17599f9a2727f3d9492c8e99
dlochmelis33 <dlochmelis@gmail.com>
2023-03-20 00:50:10 +0100

    incredible params

--- src/main/kotlin/RandSym.kt
+++ src/main/kotlin/RandSym.kt
@@ -4,6 +4,7 @@ import org.openrndr.draw.isolated
 import org.openrndr.extra.noise.Random
 import org.openrndr.extra.noise.simplex
 import org.openrndr.extra.shapes.grid
+import org.openrndr.extras.color.presets.BLUE_STEEL
 import org.openrndr.math.Matrix44
 import org.openrndr.math.transforms.transform
 import org.openrndr.shape.Rectangle
@@ -21,12 +22,10 @@ fun main() = application {
         val h = height / 10
         val grid = drawer.bounds.grid(w, h)
 
-        val colored = mutableListOf<Rectangle>()
-
         val simplexSeed = Random.int()
         val cyclePeriod = 30.0 // seconds
         val changeSpeed = 4.0
-        val fillThresh = 0.6 // from -1 to 1
+        val fillThresh = 0.65 // from -1 to 1
         val fillFreq = 1.7 // at least 1.0
         val noiseScale = 1.5
 
@@ -41,43 +40,64 @@ fun main() = application {
             return (simplex(
                 simplexSeed, x, y,
                 seconds / cyclePeriod * changeSpeed
-            // sin(t) * changeSpeed, cos(t) * changeSpeed
+                // sin(t) * changeSpeed, cos(t) * changeSpeed
             ) / SIMPLEX_ABS_LIM * fillFreq) % 1.0 > fillThresh
         }
 
+        fun isColored2(x: Double, y: Double): Boolean {
+            val t = seconds * 2.0 * PI / cyclePeriod
+            return (simplex(
+                simplexSeed + 5, x, y,
+                seconds / cyclePeriod * changeSpeed * 0.5
+                // sin(t) * changeSpeed, cos(t) * changeSpeed
+            ) / SIMPLEX_ABS_LIM * fillFreq * 0.7) % 1.0 > fillThresh + 0.2
+        }
+
         extend {
+            val colored = mutableListOf<Rectangle>()
+            val colored2 = mutableListOf<Rectangle>()
+
             val s = sqrt(w.d * h.d)
-            colored.clear()
             for (xi in 0 until w / 2) {
                 for (yi in 0 until h / 2) {
-                    if (isColored(xi / s * noiseScale, yi / s * noiseScale)) colored.add(grid[yi][xi])
+                    val x = xi / s * noiseScale
+                    val y = yi / s * noiseScale
+                    if (isColored(x, y)) colored.add(grid[yi][xi])
+                    if (isColored2(x, y)) colored2.add(grid[yi][xi])
                 }
             }
+
             drawer.isolated {
                 stroke = null
-                fill = ColorRGBa.PINK
+
+                fun doDraw() = isolated {
+//                    fill = ColorRGBa.BLUE_STEEL
+                    fill = ColorRGBa.PINK
+                    rectangles(colored)
+                    rectangles(colored2)
+                }
 
                 val mFlipX = Matrix44.IDENTITY.copy(c0r0 = -1.0)
                 val mFlipY = Matrix44.IDENTITY.copy(c1r1 = -1.0)
 
-                rectangles(colored)
+                doDraw()
                 isolated {
                     drawer.model *= transform(mFlipX) {
                         translate(-width.d, 0.0)
                     }
-                    rectangles(colored)
+                    doDraw()
                 }
                 isolated {
                     drawer.model *= transform(mFlipY) {
                         translate(0.0, -height.d)
                     }
-                    rectangles(colored)
+                    doDraw()
                 }
                 isolated {
                     drawer.model *= transform(mFlipX * mFlipY) {
                         translate(-width.d, -height.d)
                     }
-                    rectangles(colored)
+                    doDraw()
                 }
             }
         }
