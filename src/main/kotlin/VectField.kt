57ff2ccd9e4c2cf750cc3fcbc2ff87548583e7ea
dlochmelis33 <dlochmelis@gmail.com>
2023-03-27 20:44:40 +0200

    solenoidal at long last

--- src/main/kotlin/VectField.kt
+++ src/main/kotlin/VectField.kt
@@ -1,3 +1,6 @@
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.isolated
@@ -5,20 +8,30 @@ import org.openrndr.extra.noise.simplex
 import org.openrndr.extra.shapes.grid
 import org.openrndr.extras.color.presets.PURPLE
 import org.openrndr.math.Vector2
-import util.VF
-import util.div
+import org.openrndr.shape.Rectangle
+import util.*
 import kotlin.random.Random
+import kotlin.time.Duration.Companion.milliseconds
 
-private const val rows = 720 / 5
-private const val cols = 1080 / 5
+private const val rows = 720 / 15
+private const val cols = 1080 / 15
 private const val speed = 2.0
 private const val scale = 0.002
 
 private val seed = Random.nextInt()
-private fun u(x: Double, y: Double) = simplex(seed, x, y)
-private fun v(x: Double, y: Double) = simplex(seed + 1, x, y)
+//private fun u(x: Double, y: Double) = simplex(seed, x, y)
+//private fun v(x: Double, y: Double) = simplex(seed + 5, x, y)
 
-private val vf: VF =  { (x, y) -> Vector2(u(x, y) - v(y, x), v(x, y) - u(y, x)) }
+private val u: SF = { simplex(seed, it) }
+private val v: SF = { simplex(seed + 1, it) }
+
+private val vf: VF = { (x, y) ->
+//    Vector2(
+//        u(x, y) - v(y, x),
+//        v(x, y) - u(y, x)
+//    )
+    Vector2(simplex(seed + 1, y), simplex(seed + 2, x))
+}
 
 fun main() = application {
     configure {
@@ -26,24 +39,32 @@ fun main() = application {
         height = 1080
     }
     program {
-        var pts = drawer.bounds.grid(cols, rows).flatten().map { it.center }
+        val pts = Rectangle(-width.d, -height.d, width * 3.0, height * 3.0)
+            .grid(cols * 3, rows * 3).flatten().map { it.center }.toMutableList()
+        val ptsInit = pts.toList()
+
+        GlobalScope.launch {
+            while (true) {
+                pts.replaceAll {
+                    it + vf(it * scale) * speed
+                }
+                delay(1.milliseconds)
+            }
+        }
 
         extend {
-            pts = pts.map {
-                it + vf(it * scale) * speed
-            }
             drawer.circles {
                 fill = null
-                strokeWeight = 0.4
-                for (point in pts) {
+                strokeWeight = 0.1
+                for (point in ptsInit) {
                     val d = vf.div(point * scale)
                     stroke = if (d > 0) ColorRGBa.YELLOW.shade(d) else ColorRGBa.PURPLE.shade(-d)
-                    circle(point, 4.0)
+                    circle(point, 1.0)
                 }
             }
             drawer.isolated {
                 stroke = ColorRGBa.WHITE
-                strokeWeight = 1.0
+                strokeWeight = 2.0
                 points(pts)
             }
         }
--- src/main/kotlin/util/VectorField.kt
+++ src/main/kotlin/util/VectorField.kt
@@ -8,29 +8,32 @@ private const val h = 1e-6
 typealias VF = (Vector2) -> Vector2
 typealias SF = (Vector2) -> Double
 
-val VF.div: SF get() = { v ->
-    val xh = Vector2.UNIT_X * h
-    val yh = Vector2.UNIT_Y * h
-    val dxv = (this(v + xh) - this(v - xh)) / (2 * h)
-    val dyv = (this(v + yh) - this(v - yh)) / (2 * h)
-    dxv.x + dyv.y
-}
+val VF.div: SF
+    get() = { v ->
+        val xh = Vector2.UNIT_X * h
+        val yh = Vector2.UNIT_Y * h
+        val dxv = (this(v + xh) - this(v - xh)) / (2 * h)
+        val dyv = (this(v + yh) - this(v - yh)) / (2 * h)
+        dxv.x + dyv.y
+    }
 
-val VF.curl: SF get() = { v ->
-    val xh = Vector2.UNIT_X * h
-    val yh = Vector2.UNIT_Y * h
-    val dxv = (this(v + xh) - this(v - xh)) / (2 * h)
-    val dyv = (this(v + yh) - this(v - yh)) / (2 * h)
-    dxv.x - dyv.y
-}
+val VF.curl: SF
+    get() = { v ->
+        val xh = Vector2.UNIT_X * h
+        val yh = Vector2.UNIT_Y * h
+        val dxv = (this(v + xh) - this(v - xh)) / (2 * h)
+        val dyv = (this(v + yh) - this(v - yh)) / (2 * h)
+        dxv.x - dyv.y
+    }
 
-val SF.grad: VF get() = { v ->
-    val xh = Vector2(h, 0.0)
-    val yh = Vector2(0.0, h)
-    val dx = (this(v + xh) - this(v - xh)) / (2 * h)
-    val dy = (this(v + yh) - this(v - yh)) / (2 * h)
-    Vector2(dx, dy)
-}
+val SF.grad: VF
+    get() = { v ->
+        val xh = Vector2(h, 0.0)
+        val yh = Vector2(0.0, h)
+        val dx = (this(v + xh) - this(v - xh)) / (2 * h)
+        val dy = (this(v + yh) - this(v - yh)) / (2 * h)
+        Vector2(dx, dy)
+    }
 
 fun SF.normalAt(at: Vector2): Vector3 {
     val (dx, dy) = grad(at)
@@ -40,4 +43,19 @@ fun SF.normalAt(at: Vector2): Vector3 {
 val ((Double) -> Double).diff
     get() = { x: Double ->
         (this(x + h) - this(x - h)) / (2 * h)
+    }
+
+typealias SF3 = (Vector3) -> Double
+typealias VF3 = (Vector3) -> Vector3
+
+val SF3.grad: VF3
+    @JvmName("sf3grad")
+    get() = { v ->
+        val xh = Vector3.UNIT_X * h
+        val yh = Vector3.UNIT_Y * h
+        val zh = Vector3.UNIT_Z * h
+        val dx = (this(v + xh) - this(v - xh)) / (2 * h)
+        val dy = (this(v + yh) - this(v - yh)) / (2 * h)
+        val dz = (this(v + zh) - this(v - zh)) / (2 * h)
+        Vector3(dx, dy, dz)
     }
